ee7edae85f204b77ea1fb1e11c44aa54e79b8a0e
sarthak bansal <39374452+sarthakworks@users.noreply.github.com>
2024-06-26 13:39:32 +0530

    console added

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,6 +4,7 @@ export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
+    console.log(row);
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
